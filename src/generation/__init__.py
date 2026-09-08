6df180409891ec6727da4f16de5cf285b3468323
PCJ_God <3868608786@qq.com>
2026-09-08 16:43:44 +0800

    添加路由

--- src/generation/__init__.py
+++ src/generation/__init__.py
@@ -4,5 +4,6 @@
 from .llm_client import LLMClient
 from .prompt_templates import PromptTemplates
 from .answer_generator import AnswerGenerator
+from .prompt_coach import PromptCoach
 
-__all__ = ["LLMClient", "PromptTemplates", "AnswerGenerator"]
+__all__ = ["LLMClient", "PromptTemplates", "AnswerGenerator", "PromptCoach"]
